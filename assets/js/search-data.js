060cef99a9faa0df7a565a33c90fa0596f219201
Hexthol <1601661686@qq.com>
2026-05-18 08:37:43 +0000

    Deploying to gh-pages from @ Hexthol/Hexthol.github.io@a3e762b2f77e58cf2e3719a1b6456e13c764dddc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,7 +469,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/My_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
